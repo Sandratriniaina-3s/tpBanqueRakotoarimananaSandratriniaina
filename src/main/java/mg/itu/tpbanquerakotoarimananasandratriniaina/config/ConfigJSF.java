63dfe913a733a86316382cd797f9d41ee9a1df59
Sandratra <3s.sandratra@gmail.com>
2024-04-13 18:36:31 +0300

    Ajout templates

---
+++ src/main/java/mg/itu/tpbanquerakotoarimananasandratriniaina/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquerakotoarimananasandratriniaina.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author Sandratriniaina
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
