6d22b6ddb2d660d7ede22eefbe4499f9e0eb205f
Sandratra <3s.sandratra@gmail.com>
2024-04-14 11:33:51 +0300

    Traitement erreurs et messages succès

--- src/main/java/mg/itu/tpbanquerakotoarimananasandratriniaina/jsf/TransfertArgent.java
+++ src/main/java/mg/itu/tpbanquerakotoarimananasandratriniaina/jsf/TransfertArgent.java
@@ -9,6 +9,7 @@ import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import java.io.Serializable;
 import mg.itu.tpbanquerakotoarimananasandratriniaina.entity.CompteBancaire;
+import mg.itu.tpbanquerakotoarimananasandratriniaina.jsf.util.Util;
 import mg.itu.tpbanquerakotoarimananasandratriniaina.service.GestionnaireCompte;
 
 /**
@@ -54,10 +55,39 @@ public class TransfertArgent implements Serializable {
     }
 
     public String effectuerTransfert() {
+        boolean erreur = false;
         CompteBancaire cSource, cDestination;
         cSource = gestionnaireCompte.findCompteById(idSource);
+
+        if (cSource == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (cSource.getSolde() < montant) {
+                Util.messageErreur("Solde insuffisant !", "Solde insuffisant !", "form:montant");
+                erreur = true;
+            }
+        }
+
         cDestination = gestionnaireCompte.findCompteById(idDestination);
+        if (cDestination == null) {
+            // Message d'erreur associé au composant destination ; form:destination est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la destination est "destination"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+            erreur = true;
+        }
+
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
+
         gestionnaireCompte.transferer(cSource, cDestination, montant);
+        // Message de succès ; addFlash à cause de la redirection.
+        Util.addFlashInfoMessage("Transfert du montant " + montant + " de " + cSource.getNom() + " vers " + cDestination.getNom() + " effectué avec succès");
         return "listeComptes?faces-redirect=true";
     }
     
