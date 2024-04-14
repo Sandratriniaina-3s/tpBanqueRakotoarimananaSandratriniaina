8644310ca1fbe605f1c829a6c3eaeea8ddc60a6f
Sandratra <3s.sandratra@gmail.com>
2024-04-14 08:50:28 +0300

    Ajout page liste comptes et index

---
+++ src/main/java/mg/itu/tpbanquerakotoarimananasandratriniaina/jsf/ListeComptes.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquerakotoarimananasandratriniaina.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanquerakotoarimananasandratriniaina.entity.CompteBancaire;
+import mg.itu.tpbanquerakotoarimananasandratriniaina.service.GestionnaireCompte;
+
+/**
+ *
+ * @author Sandratriniaina
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+private List<CompteBancaire> allComptes;
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    public ListeComptes() {
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        if (allComptes == null) {
+            allComptes = gestionnaireCompte.getAllComptes();
+        }
+        return allComptes;
+    }
+    
+}
